7a078b23d430a99b6491922f4d801f9d4b338c24
jasdeepkaur15 <jasdeepg21@gmail.com>
2018-02-15 13:28:21 +0530

    Working apartment gem

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ Bundler.require(*Rails.groups)
 module Album
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.middleware.use Apartment::Elevators::Subdomain
     config.load_defaults 5.1
-        config.middleware.use Apartment::Elevators::Subdomain
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
