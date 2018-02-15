eb18b9a029115bc8e1de42020cd3728c4ced2657
jasdeepkaur15 <jasdeepg21@gmail.com>
2018-02-15 10:26:42 +0530

    apartment gem added

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+require 'apartment/elevators/subdomain'
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +12,7 @@ module Album
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+        config.middleware.use Apartment::Elevators::Subdomain
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
